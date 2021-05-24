43d774fafb9a08e02ceec4297f2a5564cf605dc2
s_zhangsiwei <s_zhangsiwei@kingsoft.com>
2021-05-24 10:12:09 +0800

    修改七牛云cdn

--- api/thirdparty/qiniu.go
+++ api/thirdparty/qiniu.go
@@ -16,7 +16,7 @@ import (
 const (
 	accessKey = "WeLgPYJU2FhvjgOqFp-MfPLgcybp9CSqGJ1hfeAP"
 	secretKey = "b_a8AMSaZnqEH5WVnVke1-9WvSWGfq-LVq5SESue"
-	bucket    = "promanage1"
+	bucket    = "promanage2"
 )
 
 func GetQiniuToken(c *gin.Context) {
